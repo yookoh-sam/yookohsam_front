a2531b57fe36ddeea5f98b62aeb31445436148b8
YJ <hghk0046@gmail.com>
2025-07-24 14:50:41 +0900

    feat: 잡다한 기능 추가

---
+++ vite-project/src/components/ProfileBar.jsx
@@ -0,0 +1,11 @@
+import { BackButton } from "./BackButton"
+
+export const ProfileBar = () => {
+    return(
+        <div>
+            <div className="flex flex-row pl-4">
+                <BackButton />
+            </div>
+        </div>
+    )
+}
--- vite-project/src/pages/Login.jsx
+++ vite-project/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import KakaoLogo from '../assets/KakaoTalk_logo.png';
+import { BackButton } from '../components/BackButton';
 
 export const Login = () => {
     const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
@@ -13,13 +14,17 @@ export const Login = () => {
 
     return (
             <div className="flex items-center justify-center min-h-screen bg-gray-100">
-                <div className='flex flex-col items-center justify-center w-80 h-140 rounded-xl bg-[#FFFFFF] shadow gap-20'>
-                        <div className='font-semibold text-lg px-4'>
-                            카카오톡으로 간편하게 로그인하고<p/> 서비스를 이용해보세요!
+                <div className='flex flex-col items-center w-80 h-140 rounded-xl bg-[#FFFFFF] shadow'>
+                        <BackButton/>
+                        
+                        {/* 문구 */}
+                        <div className='font-semibold text-lg px-4 text-center self-center pt-[200px]'>
+                            3초만에 로그인하고 앱이름을 <br/>200% 즐겨보세요
                         </div>
 
+                        {/* 로그인버튼 */}
                         <button 
-                        className="flex items-center bg-[#FFE808] w-70 h-14 cursor-pointer rounded-2xl"
+                        className="flex items-center bg-[#FFE808] w-70 h-14 cursor-pointer rounded-2xl mt-[180px]"
                         onClick={handleLogin}>
                             <img src={KakaoLogo} className='pl-2 pr-8 h-12'/>
                             <span className='font-semibold'>카카오톡으로 로그인하기</span> 
