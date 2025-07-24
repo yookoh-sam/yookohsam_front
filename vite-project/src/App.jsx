fab3919c188a90853c4612d0ab36ca9f86c7e34b
YJ <hghk0046@gmail.com>
2025-07-24 10:56:28 +0900

    feat: 메인페이지 세팅

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { MainPage } from './pages/MainPage'
+import { Layout } from './components/layout/Layout'
+
 function App() {
 
   return (
--- vite-project/src/pages/MainPage.jsx
+++ vite-project/src/pages/MainPage.jsx
@@ -1,7 +1,7 @@
 export const MainPage = () => {
     return(
         <div>
-            
+            메인페이지입니다.
         </div>
     )
 }
