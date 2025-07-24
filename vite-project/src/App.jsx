34fbf69d09589512d3539af6b19483c73cbc965c
haha4825 <tw086219@gmail.com>
2025-07-24 18:42:52 +0900

    hi

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,34 +1,33 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import MainPage from './pages/MainPage'
-import { Layout } from './components/layout/Layout'
-import { Login } from './pages/Login'
-import { MyPage } from './pages/MyPage'
-import KakaoRedirectPage from './pages/KakaoRedirectPage'
-import MapPage from './pages/MapPage'
-import OAuth2RedirectHandler from './pages/OAuth2RedirectHandler'
-import { ShopInfo } from './pages/ShopInfo'
-import { Profile } from './pages/Profile'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import { Layout } from "./components/layout/Layout";
+import { Login } from "./pages/Login";
+import { MyPage } from "./pages/MyPage";
+import KakaoRedirectPage from "./pages/KakaoRedirectPage";
+import MapPage from "./pages/MapPage";
+import OAuth2RedirectHandler from "./pages/OAuth2RedirectHandler";
+import ShopInfo from "./pages/ShopInfo";
+import { Profile } from "./pages/Profile";
 
 function App() {
-
   return (
-      <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<MainPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/auth/callback" element={<KakaoRedirectPage />} />
-          <Route path='/map' element={<MapPage />} />
-          <Route path='/store/:storeId' element={<ShopInfo/>}/>
-          <Route path='/mypage' element={<MyPage />}/>
-          <Route path='/profile' element={<Profile/>}/>
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/store/:storeId" element={<ShopInfo />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite-project/src/apis/store.js
+++ vite-project/src/apis/store.js
@@ -1,11 +1,12 @@
 import axiosInstance from "./axios";
 
 export const getStoreById = async (storeId) => {
-    const res = await axiosInstance.get(`/store/${storeId}`);
-    return res.data;
-}
+  const res = await axiosInstance.get(`/store/${storeId}`);
+  console.log(res.data);
+  return res.data;
+};
 
 export const getReviewById = async (storeId) => {
-    const res = await axiosInstance.get(`/store/${storeId}/review`);
-    return res.data;
-}
+  const res = await axiosInstance.get(`/store/${storeId}/reviews`);
+  return res.data;
+};
