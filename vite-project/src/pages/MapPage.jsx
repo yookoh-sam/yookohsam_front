25a0bd8acc49be61ed83fac7981d294b9bf2cad1
Juyoung03 <iris031008@naver.com>
2025-07-24 11:30:25 +0900

    feat: 지도 띄우기

--- vite-project/src/pages/MapPage.jsx
+++ vite-project/src/pages/MapPage.jsx
@@ -1,6 +1,43 @@
+import { useEffect, useRef, useState } from "react";
+
 const MapPage = () => {
+    const mapRef = useRef(null);
+    const [map, setMap] = useState(null);
+    const [isTmapReady, setIsTmapReady] = useState(false);
+
+    useEffect(() => {
+        const checkInterval = setInterval(() => {
+            if (window.Tmapv2) {
+                setIsTmapReady(true);
+                clearInterval(checkInterval);
+            }
+        }, 100);
+        return () => clearInterval(checkInterval);
+    }, []);
+
+    useEffect(() => {
+    if (isTmapReady && mapRef.current) {
+      const mapInstance = new window.Tmapv2.Map(mapRef.current, {
+        center: new window.Tmapv2.LatLng(37.5526836958422, 126.92500844858347),
+        width: "100%",
+        height: "480px",
+        zoom: 18,
+        // scrollwheel: false,
+        // zoomControl: false,
+        draggable: true,
+      });
+      setMap(mapInstance);
+
+    }
+  }, [isTmapReady]);
+
     return (
-        <div>map</div>
+        <div>
+            <div
+                ref={mapRef}
+                className="w-full h-[480px]"    
+            ></div>
+        </div>
     )
 }
 
